965b2f699a461c3f9dd53489488c8bd7839a22d5
Maida Afzal <72850994+mydafzal@users.noreply.github.com>
2021-08-08 20:52:51 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,13 @@ const Footer = () => {
             <a href="mailto:hello@ian.dev" id="contact" className="footer__link">uni.m.souban@outlook.com</a>
             <ul className="social-list">
                 <li className="social-list__item">
-                    <a className="social-list__link" href="https://www.instagram.com/maidansiddique">
+                    <a className="social-list__link" href="https://www.instagram.com/souban99">
                         <i className="fab fa-instagram"></i>
                     </a>
                 </li>
+               
                 <li className="social-list__item">
-                    <a className="social-list__link" href="http://dribbble.com">
-                        <i className="fab fa-dribbble"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a className="social-list__link" href="https://twitter.com">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a className="social-list__link" href="https://github.com/maidansiddique">
+                    <a className="social-list__link" href="https://github.com/soubanmuhammad">
                         <i className="fab fa-github"></i>
                     </a>
                 </li>
